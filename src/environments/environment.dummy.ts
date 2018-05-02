79a9c3f5e9f7c19edd003de643c9b20d2390bcf1
Joakim Mattsson <joamat91@gmail.com>
2018-05-02 09:00:23 +0200

    added dummy file

---
+++ src/environments/environment.dummy.ts
@@ -0,0 +1,14 @@
+/*
+    This is a dummy file to represent the files you need to make the project work as intended.
+
+    Instead of this file you need two files called
+        - environment.ts
+        - environment.prod.ts
+
+    Also don't forget to add key for OpenWeatherMap
+
+*/
+export const environment = {
+    production: false,
+    owmKey: 'Api key to OpenWeatherMap'
+  };
