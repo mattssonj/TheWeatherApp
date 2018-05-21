55d0b003bf967fe512389db5eeada7776190b369
Joakim Mattsson <joamat91@gmail.com>
2018-05-22 00:32:29 +0200

    fixed buttons to press forward and backward to change day. Added hourly forecast to screen

--- src/app/weather/forecast.ts
+++ src/app/weather/forecast.ts
@@ -3,8 +3,9 @@ import { Wind } from './wind';
 import { Main } from './main';
 
 export interface Forecast {
-    name: string;
+    name: string; // only used with currentWeather
     weather: Weather[];
     wind: Wind;
     main: Main;
+    dt_txt: string; // only used with five-forecast! showing date of forecast
 }
